2508c5e44b04e3d6925977912a1aff534f1b96ab
Erdni <64160883+ErdniElzenov@users.noreply.github.com>
2022-12-18 23:45:17 +0300

    first commit

---
+++ clients/js/script.js
@@ -0,0 +1,32 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+let modal = document.getElementsById('id01');
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const nodemailer = require("nodemailer");  
+const app = express();
+const urlencodedParser = express.urlencoded({extended: false});
+app.use(express.static('../clients'))
+
+
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '../clients/index.html')
+});
+
+
+app.post("/", urlencodedParser, function (request, response) {
+  if(!request.body) return response.sendStatus(400);
+  console.log(request.body);
+
+  async function main(textMail){
+    let transporter = nodemailer.createTransport({
+    host: "smtp.mail.ru",
+    port: 465,
+    secure: true, 
+    auth: {
+      user: "prochinalogistics@mail.ru", 
+      pass: "BPESD0KcAbsnvkQQDJyx" 
+    }
+  });
+      
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"prochinalogistics@mail.ru', // sender address
+    to: "erdnielzenov11@gmail.com", // list of receivers
+    subject: "Здравствуйте " , // Subject line
+    text: textMail, // plain text body
+    html: "<h2>" + request.body.userName + "<h2> <br>" + "<h2>Телефон " + request.body.userPhone + "<h2> <br>" + "<h2>Почта " + request.body.userEmail + "<h2>" ,// html body
+  });
+
+
+
+}
+
+main(destinyArry[fate[4] - 1]).catch(console.error);
+
+response.sendFile(__dirname + 'данные успешно отправлены')
+});
+
+app.listen(8082, ()=>console.log("Сервер запущен...")); 
